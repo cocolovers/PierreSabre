579f3fd36c964706b87fdd4be77e2afccc755d07
Minh-dieu Nguyen <ngd5603a@ensfsi.onmicrosoft.com>
2025-11-25 17:31:59 +0100

    Created class Commercant

---
+++ src/personnages/Commercant.java
@@ -0,0 +1,20 @@
+package personnages;
+
+public class Commercant extends Humain {
+
+	public Commercant(String nom, String boisson, int argent) {
+		super(nom, boisson, argent);
+	}
+	public int seFaireExtorquer() {
+		int sommeperdue = argent;
+		this.argent -= argent;
+		parler("J'ai tout perdu! Le monde est trop injuste... ");
+		return sommeperdue;
+	}
+	
+	public void recevoir(int argent) {
+		this.argent += argent;
+		parler(argent + " sous! Je te remercie genereux donateur !");
+	}
+
+}
